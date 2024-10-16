dbdbf00a0b8fa11d00c00931d5fe6fc9a26cbf5f
stvy2346 <stvycoding@gmail.com>
2024-10-16 20:45:34 +0530

    changed authRoutes to auth.route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-import authRoutes from './routes/authRoutes.js';
+import authRoutes from './routes/auth.route.js';
 import connectMongoDB from './db/connectMongoDB.js';
 
 dotenv.config();
