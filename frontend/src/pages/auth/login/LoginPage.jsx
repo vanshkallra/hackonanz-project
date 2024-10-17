ad5d5ac98c4e900b6d28c53c3808e9e0ecb2f2f5
Vansh Kalra <139179469+vanshkallra@users.noreply.github.com>
2024-10-17 10:00:39 +0530

    Update LoginPage.jsx

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
 	} = useMutation({
 		mutationFn: async ({ username, password }) => {
 			try {
-				const res = await fetch("/api/auth/login", {
+				const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/auth/login`, {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
